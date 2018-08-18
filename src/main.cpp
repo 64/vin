c64f8effaa2e49904504c1095608213013e62327
Crally <jameswhitfield65@gmail.com>
2018-08-18 02:00:33 -0500

    Simple generalized error function in main for initilizations

--- src/main.cpp
+++ src/main.cpp
@@ -14,18 +14,17 @@
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void process_input(GLFWwindow *window);
 void message_box(const std::string& title, const std::string& msg);
+[[noreturn]] void error(const std::string& msg);
 
 constexpr unsigned int SCR_WIDTH = 800;
 constexpr unsigned int SCR_HEIGHT = 600;
 
-int main()
+int main(int, char**)
 {
 #ifdef __DEBUG__
     std::cout << "Vin Editor Debug Build" << std::endl;
     std::cout << "Compiled on " << __DATE__ << " at " << __TIME__ << std::endl;
 #endif
-
-//    message_box("Crally", "This is a message box with X11");
 
     ConfigReader config;
     config.load();
@@ -40,31 +39,21 @@ int main()
 
     GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Vin Editor", nullptr, nullptr);
     if (!window)
-    {
-        std::cerr << "Failed to create GLFW window" << std::endl;
-        glfwTerminate();
-        return EXIT_FAILURE;
-    }
+        error("Failed to create GLFW window");
 
     glfwMakeContextCurrent(window);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 	glfwSwapInterval(1);
 
     if (!gladLoadGLLoader(reinterpret_cast<GLADloadproc>(glfwGetProcAddress)))
-    {
-        std::cerr << "Failed to initialize GLAD" << std::endl;
-        return EXIT_FAILURE;
-    }
+        error("Failed to initialize GLAD");
 
     FT_Library library;
     if (FT_Init_FreeType(&library))
-    {
-        std::cerr << "Failed to initialize Freetype" << std::endl;
-        return EXIT_FAILURE;
-    }
+        error("Failed to initialize Freetype");
 
     Renderer renderer;
-    Font font(library, config.option("font_path"), 100);
+    Font font(library, config.option("font_path"), 12);
 
     while (!glfwWindowShouldClose(window))
     {
@@ -82,6 +71,13 @@ int main()
     FT_Done_FreeType(library);
     glfwTerminate();
     return EXIT_SUCCESS;
+}
+
+void error(const std::string& msg)
+{
+    std::cerr << msg << std::endl;
+    glfwTerminate();
+    exit(EXIT_FAILURE);
 }
 
 void process_input(GLFWwindow *window)
